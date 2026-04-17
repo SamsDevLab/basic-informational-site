19a5e4043fc59541c47e6e3040e80613b113bbfc
Sam Hadfield <sthtech@protonmail.com>
2026-04-17 11:54:08 -0500

    feat: add server and routing logic

---
+++ index.js
@@ -0,0 +1,41 @@
+import http from "node:http";
+import fs from "node:fs";
+
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./site/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    case "/about":
+      path += "about.html";
+      res.statusCode = 200;
+      break;
+    case "/contact":
+      path += "contact-me.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.write(data);
+      res.end();
+    }
+  });
+});
+
+server.listen(8080, () => {
+  console.log("Listening on port 8080");
+});
